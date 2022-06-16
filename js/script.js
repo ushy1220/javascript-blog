d2773b139eb0b6ae5d5677a2c3b4b54d089a0d36
Michał Bielecki <mb9246@gmail.com>
2022-06-16 19:02:45 +0200

    try to add and remove classes active on/from links and articles

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 function titleClickHandler(event){
   const clickedElement = this;
+  event.preventDefault();         //moje moje moje 
   console.log('Link was clicked!');
   console.log(event);
 
@@ -15,11 +16,15 @@ for(let activeLink of activeLinks){
 
   /* [IN PROGRESS] add class 'active' to the clicked link */
 
+for(let link of links) {                         //MOJE
+  link.classList.add('active');         //MOJE
+}
+
   console.log('clickedElement:', clickedElement);
 
   /* remove class 'active' from all articles */
 
-  const activeArticles = document.querySelectorAll('.titles a.active');
+  const activeArticles = document.querySelectorAll('.posts a.active');
 
 for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
@@ -30,10 +35,16 @@ for(let activeArticle of activeArticles){
   /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
+
+  const articlesSelector = document.getElementsBy('href');   //MOJE
+  for(let article of articles){        //MOJE
+    articleSelector.classList.add('active');  //MOJE
+  }
+  article.classList.remove('active');
 }
 
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
-}
+} 
