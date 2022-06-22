ca890fa51adf2cda31409ba41a688cc7b892c88a
Michał Bielecki <mb9246@gmail.com>
2022-06-22 10:40:35 +0200

    samodzielnie ale nie dziala

--- js/script.js
+++ js/script.js
@@ -107,45 +107,59 @@ function generateTags(){
 
 generateTags();
 
-function tagClickHandler(event){
+function tagClickHandler(event){            //SAMODZIELNIE
 
   /* prevent default action for this event */
   event.preventDefault
+
   /* make new constant named "clickedElement" and give it the value of "this" */
+  const clickedElement = this;
 
   /* make a new constant "href" and read the attribute "href" of the clicked element */
+  const href = clickedElement.getAttribute('href');
 
   /* make a new constant "tag" and extract tag from the "href" constant */
+  const tag = href.getAttribute('data-tags');
 
   /* find all tag links with class active */
+  let tagLinks = tag.classList('active');
 
   /* START LOOP: for each active tag link */
+  for(let tagLink of tagLinks){
 
     /* remove class active */
+    tagLink.classList.remove('active');
 
   /* END LOOP: for each active tag link */
+  }
 
   /* find all tag links with "href" attribute equal to the "href" constant */
+  let equalLinks = href == articles.getAttribute('href');
 
   /* START LOOP: for each found tag link */
+  for(let equalLink of equalLinks){
 
     /* add class active */
+    equalLink.classList.add('avtive');
 
   /* END LOOP: for each found tag link */
+  }
 
   /* execute function "generateTitleLinks" with article selector as argument */
+  generateTitleLinks.querySelector(article);
 }
 
-        // Wstawic TU!!!!
-
-        
 function addClickListenersToTags(){
+
   /* find all links to tags */
   let links = document.querySelectorAll('a[href^="#tag-"]'); // (optArticleTagsSelector + ' a')
+
   /* START LOOP: for each link */
   for(let link of links){
+
     /* add tagClickHandler as event listener for that link */
     link.addEventListener('click', tagClickHandler);
+
   /* END LOOP: for each link */
   }
 }
